0179e3921dae1780e1f43a3038193b93664629ee
Thomas Chisholm <thomas.chisholm@puppet.com>
2023-09-25 15:42:13 -0700

    Pull down installation tarball for 2021.7.5 and unzip

--- resources/scripts/installer.sh
+++ resources/scripts/installer.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env sh
+
+set -x
+
+# Install dependencies
+sudo dnf update -y
+#change directories to /tmp
+cd /tmp
+#Download Puppet Enterprise 2021.7.5
+sudo curl -JLO 'https://pm.puppetlabs.com/puppet-enterprise/2021.7.5/puppet-enterprise-2021.7.5-el-8-x86_64.tar.gz'
+#Unzip the tarball
+sudo tar -xvf puppet-enterprise-2021.7.5-el-8-x86_64.tar.gz
