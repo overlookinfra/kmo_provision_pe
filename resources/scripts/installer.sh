48b53f9bc020de1a1e7f6a81e5305c13d99a83da
Jain Waldrip <jain.waldrip@perforce.com>
2023-10-19 11:52:55 -0500

    Clean up and prettify

--- resources/scripts/installer.sh
+++ resources/scripts/installer.sh
@@ -3,16 +3,11 @@
 set -x
 
 # Prepare /etc/hosts
-sudo sed -i '/^127\.0\.0\.1/ s/$/ puppet/' /etc/hosts
-sudo sed -i '/^::1/ s/$/ puppet/' /etc/hosts
 sudo sed -i 's/\(^[0-9.]*\).*\.c\.kmo-instruqt\.internal/\1 puppet.c.kmo-instruqt.internal/' /etc/hosts
 sudo sed -i 's/\(puppet\.c\.kmo-instruqt\.internal\).*/\1 puppet/' /etc/hosts
 
-
 #Change the hostname to the FQDN of the master
 sudo hostnamectl set-hostname puppet
-
-
 
 #change directories to /tmp
 cd /tmp
@@ -21,14 +16,12 @@ sudo curl -JLO 'https://pm.puppetlabs.com/puppet-enterprise/2021.7.5/puppet-ente
 #Unzip the tarball
 sudo tar -xvf puppet-enterprise-2021.7.5-el-8-x86_64.tar.gz
 #install puppet enterprise
-sudo ./puppet-enterprise-2021.7.5-el-8-x86_64/puppet-enterprise-installer -c /tmp/puppet-enterprise-2021.7.5-el-8-x86_64/conf.d/pe.conf
+sudo /tmp/puppet-enterprise-2021.7.5-el-8-x86_64/puppet-enterprise-installer -c /tmp/puppet-enterprise-2021.7.5-el-8-x86_64/conf.d/pe.conf
 
 #Need to run puppet agent as root twice to get the cert signed
 sudo /opt/puppetlabs/bin/puppet agent -t
 sudo /opt/puppetlabs/bin/puppet agent -t
 
-
 #Set the console password
 sudo /opt/puppetlabs/bin/puppet infrastructure console_password --password 'puppetlabs'
-
 
